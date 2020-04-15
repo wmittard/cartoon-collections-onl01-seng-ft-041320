0b2d3b08a4f56c9eec292c5e48a327632af713c5
William Mittard <billymittard@ymail.com>
2020-04-15 22:44:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,15 +5,14 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(veggies) 
-    veggies.collect do |call| 
+def summon_captain_planet(calls) 
+    calls.collect do |call| 
     call.capitalize + "!"
   end
-
 end
 
-def long_planeteer_calls(calls)
-  calls.any? do |call|
+def long_planeteer_calls(long_calls)
+  long_calls.any? do |call|
     call.length > 4
   end
 end
