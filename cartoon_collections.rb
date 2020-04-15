c4dec4be8b69d25a91568ed3cc30ed532210111f
William Mittard <billymittard@ymail.com>
2020-04-15 22:35:41 +0000

    Automatically backed up by Learn

---
+++ cartoon_collections.rb
@@ -0,0 +1,32 @@
+
+def roll_call_dwarves(dwarfs)
+  dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarfs.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
+end
+
+def summon_captain_planet(veggies) 
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
+  end
+
+end
+
+def long_planteer_calls(long_planteer_calls)
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
+end
+
+def find_the_cheese(cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
