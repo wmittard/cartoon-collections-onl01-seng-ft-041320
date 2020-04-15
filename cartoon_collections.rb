7ce14918b8727f763e96e2093341fda6d42e524a
William Mittard <billymittard@ymail.com>
2020-04-15 22:38:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,15 +12,7 @@ def summon_captain_planet(veggies)
 
 end
 
-def long_planteer_calls(long_planteer_calls)
-  answer = false
-  long_planteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-    answer
-end
+
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
