d311e7d96b19fd107e6c99570b52c8a53b95bad5
William Mittard <billymittard@ymail.com>
2020-04-15 22:33:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     puts "#{index + 1} #{name}"
-  
-end
+  end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
